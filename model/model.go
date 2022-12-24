9182ed5a776bcfcb57a6536069ebb1262f655533
steve <ionc635@naver.com>
2022-12-24 15:27:07 +0900

    add: Model 설계 및 구현

--- model/model.go
+++ model/model.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"context"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -10,6 +11,51 @@ import (
 type Model struct {
 	client   *mongo.Client
 	colOrder *mongo.Collection
+}
+
+type Menu struct {
+	Name           string    `bson:"name"`
+	CanBeOrder     bool      `bson:"can_be_order"`
+	Limited        int       `bson:"limited"`
+	Price          int       `bson:"price"`
+	Origin         string    `bson:"origin"`
+	TodayRecommend bool      `bson:"today_recommend"`
+	CreatedAt      time.Time `bson:"created_at"`
+	UpdatedAt      time.Time `bson:"updated_at"`
+}
+
+type Orderer struct {
+	Name      string    `bson:"name"`
+	Phone     string    `bson:"phone"`
+	Address   string    `bson:"address"`
+	CreatedAt time.Time `bson:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at"`
+}
+
+type MenuReview struct {
+	Menu          Menu      `bson:"menu"`
+	Orderer       Orderer   `bson:"orderer"`
+	Score         int       `bson:"score"`
+	IsRecommended bool      `bson:"is_recommended"`
+	review        string    `bson:"review"`
+	CreatedAt     time.Time `bson:"created_at"`
+	UpdatedAt     time.Time `bson:"updated_at"`
+}
+
+type Order struct {
+	Menu      Menu      `bson:"menu"`
+	Orderer   Orderer   `bson:"orderer"`
+	State     string    `bson:"state"`
+	Numbering int       `bson:"numbering"`
+	CreatedAt time.Time `bson:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at"`
+}
+
+type Reorder struct {
+	Menu      Menu      `bson:"menu"`
+	Orderer   Orderer   `bson:"orderer"`
+	CreatedAt time.Time `bson:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at"`
 }
 
 func NewModel() (*Model, error) {
